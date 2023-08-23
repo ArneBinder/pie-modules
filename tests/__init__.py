bdf196c7a0ccf9fc0eb58050d5b06898da3de4fe
Arne Binder <arne.binder@dfki.de>
2023-08-23 16:57:40 +0200

    improve tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,10 @@
 import pathlib
+from typing import Any, Dict
 
 TESTS_ROOT = pathlib.Path(__file__).parent
 FIXTURES_ROOT = TESTS_ROOT / "fixtures"
+
+
+def _config_to_str(cfg: Dict[str, Any]) -> str:
+    result = "-".join([f"{k}={cfg[k]}" for k in sorted(cfg)])
+    return result
