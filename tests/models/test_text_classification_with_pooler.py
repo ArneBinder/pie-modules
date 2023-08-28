5db260bb29b78883f40cc0e0b22322fc955eac27
Arne Binder <arne.binder@dfki.de>
2023-08-28 18:59:52 +0200

    do not use any documents and taskmodule in test_text_classification_with_pooler.py to decrease runtime

--- tests/models/test_text_classification_with_pooler.py
+++ tests/models/test_text_classification_with_pooler.py
@@ -4,56 +4,232 @@ import transformers
 from transformers.modeling_outputs import BaseModelOutputWithPooling
 
 from pie_models.models import TextClassificationModelWithPooler
-from pie_models.taskmodules import RETextClassificationWithIndicesTaskModule
-
-
-@pytest.fixture(scope="session")
-def documents(dataset):
-    return dataset["train"]
-
-
-@pytest.fixture(scope="module")
-def taskmodule():
-    tokenizer_name_or_path = "bert-base-cased"
-    taskmodule = RETextClassificationWithIndicesTaskModule(
-        tokenizer_name_or_path=tokenizer_name_or_path,
-        add_argument_indices_to_input=True,
-    )
-    return taskmodule
 
 
 @pytest.fixture
-def prepared_taskmodule(taskmodule, documents):
-    taskmodule.prepare(documents)
-    return taskmodule
+def inputs():
+    result_dict = {
+        "input_ids": torch.tensor(
+            [
+                [
+                    101,
+                    28998,
+                    13832,
+                    3121,
+                    2340,
+                    138,
+                    28996,
+                    1759,
+                    1120,
+                    28999,
+                    139,
+                    28997,
+                    119,
+                    102,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                ],
+                [
+                    101,
+                    1752,
+                    5650,
+                    119,
+                    28998,
+                    13832,
+                    3121,
+                    2340,
+                    144,
+                    28996,
+                    1759,
+                    1120,
+                    28999,
+                    145,
+                    28997,
+                    119,
+                    1262,
+                    1771,
+                    146,
+                    119,
+                    102,
+                    0,
+                ],
+                [
+                    101,
+                    1752,
+                    5650,
+                    119,
+                    28998,
+                    13832,
+                    3121,
+                    2340,
+                    144,
+                    28996,
+                    1759,
+                    1120,
+                    145,
+                    119,
+                    1262,
+                    1771,
+                    28999,
+                    146,
+                    28997,
+                    119,
+                    102,
+                    0,
+                ],
+                [
+                    101,
+                    1752,
+                    5650,
+                    119,
+                    13832,
+                    3121,
+                    2340,
+                    144,
+                    1759,
+                    1120,
+                    28999,
+                    145,
+                    28997,
+                    119,
+                    1262,
+                    1771,
+                    28998,
+                    146,
+                    28996,
+                    119,
+                    102,
+                    0,
+                ],
+                [
+                    101,
+                    1752,
+                    5650,
+                    119,
+                    28998,
+                    13832,
+                    3121,
+                    2340,
+                    150,
+                    28996,
+                    1759,
+                    1120,
+                    28999,
+                    151,
+                    28997,
+                    119,
+                    1262,
+                    1122,
+                    1771,
+                    152,
+                    119,
+                    102,
+                ],
+                [
+                    101,
+                    1752,
+                    5650,
+                    119,
+                    13832,
+                    3121,
+                    2340,
+                    150,
+                    1759,
+                    1120,
+                    151,
+                    119,
+                    1262,
+                    28998,
+                    1122,
+                    28996,
+                    1771,
+                    28999,
+                    152,
+                    28997,
+                    119,
+                    102,
+                ],
+                [
+                    101,
+                    1752,
+                    5650,
+                    119,
+                    13832,
+                    3121,
+                    2340,
+                    150,
+                    1759,
+                    1120,
+                    151,
+                    119,
+                    1262,
+                    28999,
+                    1122,
+                    28997,
+                    1771,
+                    28998,
+                    152,
+                    28996,
+                    119,
+                    102,
+                ],
+            ]
+        ),
+        "attention_mask": torch.tensor(
+            [
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            ]
+        ),
+        "pooler_start_indices": torch.tensor(
+            [[2, 10], [5, 13], [5, 17], [17, 11], [5, 13], [14, 18], [18, 14]]
+        ),
+        "pooler_end_indices": torch.tensor(
+            [[6, 11], [9, 14], [9, 18], [18, 12], [9, 14], [15, 19], [19, 15]]
+        ),
+    }
 
-
-class MockConfig:
-    def __init__(self, hidden_size: int = 10, classifier_dropout: float = 1.0) -> None:
-        self.hidden_size = hidden_size
-        self.classifier_dropout = classifier_dropout
-
-
-class MockModel:
-    def __init__(self, batch_size, seq_len, hidden_size) -> None:
-        self.batch_size = batch_size
-        self.seq_len = seq_len
-        self.hidden_size = hidden_size
-
-    def __call__(self, *args, **kwargs):
-        last_hidden_state = torch.rand(self.batch_size, self.seq_len, self.hidden_size)
-        return BaseModelOutputWithPooling(last_hidden_state=last_hidden_state)
-
-    def resize_token_embeddings(self, new_num_tokens):
-        pass
+    return result_dict
 
 
 @pytest.fixture(params=["cls_token", "mention_pooling", "start_tokens"])
-def mock_model(monkeypatch, documents, prepared_taskmodule, request):
-    encodings = prepared_taskmodule.encode(documents, encode_target=True)
-    inputs, _ = prepared_taskmodule.collate(encodings)
+def pooler_type(request):
+    return request.param
 
-    batch_size, seq_len = inputs["input_ids"].shape
+
+@pytest.fixture
+def model(monkeypatch, pooler_type, inputs):
+    class MockConfig:
+        def __init__(self, hidden_size: int = 10, classifier_dropout: float = 1.0) -> None:
+            self.hidden_size = hidden_size
+            self.classifier_dropout = classifier_dropout
+
+    class MockModel:
+        def __init__(self, batch_size, seq_len, hidden_size) -> None:
+            self.batch_size = batch_size
+            self.seq_len = seq_len
+            self.hidden_size = hidden_size
+
+        def __call__(self, *args, **kwargs):
+            last_hidden_state = torch.rand(self.batch_size, self.seq_len, self.hidden_size)
+            return BaseModelOutputWithPooling(last_hidden_state=last_hidden_state)
+
+        def resize_token_embeddings(self, new_num_tokens):
+            pass
+
+    batch_size = inputs["input_ids"].shape[0]
+    seq_len = inputs["input_ids"].shape[1]
     hidden_size = 10
     num_classes = 4
     tokenizer_vocab_size = 30000
@@ -71,32 +247,30 @@ def mock_model(monkeypatch, documents, prepared_taskmodule, request):
         ),
     )
 
-    model = TextClassificationModelWithPooler(
+    result = TextClassificationModelWithPooler(
         model_name_or_path="some-model-name",
         num_classes=num_classes,
         tokenizer_vocab_size=tokenizer_vocab_size,
         ignore_index=0,
-        pooler=request.param,
+        pooler=pooler_type,
         # disable warmup because it would require a trainer and a datamodule to get the total number of training steps
         warmup_proportion=0.0,
     )
-    assert not model.is_from_pretrained
+    assert not result.is_from_pretrained
 
-    return model
+    return result
 
 
-def test_forward(documents, prepared_taskmodule, mock_model):
-    encodings = prepared_taskmodule.encode(documents, encode_target=True)
-    inputs, _ = prepared_taskmodule.collate(encodings)
-
-    output = mock_model.forward(inputs)
+def test_forward(inputs, model):
+    batch_size, seq_len = inputs["input_ids"].shape
+    output = model.forward(inputs)
 
     assert set(output.keys()) == {"logits"}
-    assert output["logits"].shape[0] == len(encodings)
+    assert output["logits"].shape[0] == batch_size
     assert output["logits"].shape[1] == 4  # num classes is set to 4 in mock_model()
-    if mock_model.pooler_config["type"] == "cls_token":
-        assert mock_model.classifier.in_features == 10  # hidden size = 10 in mock_model()
-    elif mock_model.pooler_config["type"] in ["mention_pooling", "start_tokens"]:
+    if model.pooler_config["type"] == "cls_token":
+        assert model.classifier.in_features == 10  # hidden size = 10 in mock_model()
+    elif model.pooler_config["type"] in ["mention_pooling", "start_tokens"]:
         assert (
-            mock_model.classifier.in_features == 20
+            model.classifier.in_features == 20
         )  # hidden size = 10 in mock_model(), *2 for concat
