e7b59c0a26274d2462ecc3ec86ea1c1c0cb9853f
Arne Binder <arne.binder@dfki.de>
2023-10-09 18:24:43 +0200

    export model and taskmodule

--- src/pie_modules/taskmodules/__init__.py
+++ src/pie_modules/taskmodules/__init__.py
@@ -1,3 +1,4 @@
+from .extractive_question_answering import ExtractiveQuestionAnsweringTaskModule
 from .re_text_classification_with_indices import (
     RETextClassificationWithIndicesTaskModule,
 )
