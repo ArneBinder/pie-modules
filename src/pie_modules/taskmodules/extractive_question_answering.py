d33e12168354df9d809fc5e4a47e0c2cdca3a0c1
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2023-11-30 20:48:59 +0530

    import tokenize_document from pie_modules

--- src/pie_modules/taskmodules/extractive_question_answering.py
+++ src/pie_modules/taskmodules/extractive_question_answering.py
@@ -4,7 +4,6 @@ from typing import Any, Dict, Iterator, List, Optional, Sequence, Tuple, Union
 
 import numpy as np
 import torch
-from pytorch_ie import tokenize_document
 from pytorch_ie.core import Annotation, AnnotationList, TaskEncoding, TaskModule
 from pytorch_ie.documents import TextBasedDocument
 from tokenizers import Encoding
@@ -12,6 +11,7 @@ from transformers import AutoTokenizer, BatchEncoding, PreTrainedTokenizer
 from transformers.modeling_outputs import QuestionAnsweringModelOutput
 from typing_extensions import TypeAlias
 
+from pie_modules.document.processing import tokenize_document
 from pie_modules.documents import (
     ExtractiveAnswer,
     ExtractiveQADocument,
