2482a0e874c6239ffb715c854ff5f7a1e1088168
Arne Binder <arne.binder@dfki.de>
2023-12-01 11:28:43 +0100

    re-export metrics (and statistics) from pytorch-ie

--- src/pie_modules/metrics/__init__.py
+++ src/pie_modules/metrics/__init__.py
@@ -1,1 +1,9 @@
+from pytorch_ie.metrics import F1Metric
+from pytorch_ie.metrics.statistics import (
+    FieldLengthCollector,
+    LabelCountCollector,
+    SubFieldLengthCollector,
+    TokenCountCollector,
+)
+
 from .span_length_collector import SpanLengthCollector
