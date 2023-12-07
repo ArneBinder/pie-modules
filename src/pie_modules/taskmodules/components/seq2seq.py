7a30a7e9b138840db260316ecaa558ed61257602
Arne Binder <arne.binder@dfki.de>
2023-12-07 23:38:10 +0100

    further disentangle annotation encoder decoder from taskmodule

--- src/pie_modules/taskmodules/components/seq2seq.py
+++ src/pie_modules/taskmodules/components/seq2seq.py
@@ -1,13 +1,16 @@
 import abc
+import inspect
 import logging
 from collections import Counter, defaultdict
 from functools import cmp_to_key
 from itertools import chain
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Sequence, Set, Tuple
 
 import numpy as np
+from pytorch_ie import Document, PreparableMixin
 from pytorch_ie.annotations import BinaryRelation, LabeledSpan, Span
 from pytorch_ie.core import Annotation
+from pytorch_lightning.core.mixins import HyperparametersMixin
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +28,10 @@ class SpanEncoderDecoder(abc.ABC):
 
 
 class AnnotationEncoderDecoder(abc.ABC):
-    layer_names: List[str]
+    @property
+    @abc.abstractmethod
+    def layer_names(self) -> List[str]:
+        pass
 
     @abc.abstractmethod
     def encode(
@@ -66,20 +72,23 @@ class SimpleSpanEncoderDecoder(SpanEncoderDecoder):
         return [span.start, span.end]
 
     def decode(self, targets: List[int], metadata: Optional[Dict[str, Any]] = None) -> Span:
-        assert len(targets) == 2
+        if len(targets) != 2:
+            raise Exception(
+                f"two target values are required to decode as Span, but targets is: {targets}"
+            )
         return Span(start=targets[0], end=targets[1])
 
 
-class PointerNetworkSpanEncoderDecoder(SpanEncoderDecoder):
-    def __init__(self, span_end_mode: str, pointer_offset: int = 0):
+class SpanEncoderDecoderWithOffset(SpanEncoderDecoder):
+    def __init__(self, offset: int, span_end_mode: str = "last_token"):
         self.span_end_mode = span_end_mode
-        self.pointer_offset = pointer_offset
+        self.offset = offset
 
     def encode(self, span: Span, metadata: Optional[Dict[str, Any]] = None) -> Optional[List[int]]:
         if self.span_end_mode == "first_token_of_last_word":
             raise NotImplementedError("span_end_mode=first_token_of_last_word not implemented")
         elif self.span_end_mode == "last_token":
-            return [span.start + self.pointer_offset, span.end + self.pointer_offset - 1]
+            return [span.start + self.offset, span.end + self.offset - 1]
         else:
             raise Exception(f"unknown span_end_mode: {self.span_end_mode}")
 
@@ -92,57 +101,141 @@ class PointerNetworkSpanEncoderDecoder(SpanEncoderDecoder):
         if self.span_end_mode == "first_token_of_last_word":
             raise NotImplementedError("span_end_mode=first_token_of_last_word not implemented")
         elif self.span_end_mode == "last_token":
-            return Span(
-                start=targets[0] - self.pointer_offset, end=targets[1] - self.pointer_offset + 1
-            )
+            return Span(start=targets[0] - self.offset, end=targets[1] - self.offset + 1)
         else:
             raise Exception(f"unknown span_end_mode: {self.span_end_mode}")
 
 
-class PointerNetworkSpanAndRelationEncoderDecoder(PointerNetworkEncoderDecoder):
-    span_layer_name = "span"
-    relation_layer_name = "relation"
-    layer_names = [span_layer_name, relation_layer_name]
+class PointerNetworkSpanAndRelationEncoderDecoder(
+    PointerNetworkEncoderDecoder,
+    PreparableMixin,
+):
+    PREPARED_ATTRIBUTES = ["labels_per_layer", "bos_token", "eos_token"]
 
     def __init__(
         self,
-        id2label: Dict[int, str],
-        bos_id: int,
-        eos_id: int,
-        span_ids: List[int],
-        relation_ids: List[int],
-        none_id: int,
+        bos_token: str,
+        eos_token: str,
+        span_layer_name: str = "labeled_spans",
+        relation_layer_name: str = "binary_relations",
+        none_label: str = "none",
         loop_dummy_relation_name: str = "loop",
         ignore_error_types: Optional[List[str]] = None,
-        span_encoder_decoder: Optional[SpanEncoderDecoder] = None,
+        span_encoder_decoder_name: str = "span_encoder_decoder_with_offset",
+        span_encoder_decoder_kwargs: Optional[Dict[str, Any]] = None,
+        labels_per_layer: Optional[Dict[str, List[str]]] = None,
+        exclude_labels_per_layer: Optional[Dict[str, List[str]]] = None,
     ):
-        self.span_encoder_decoder = span_encoder_decoder or SimpleSpanEncoderDecoder()
-        self.id2label = id2label
-        self.label2id: Dict[str, int] = {v: k for k, v in id2label.items()}
+        self.labels_per_layer = labels_per_layer
+        self.exclude_labels_per_layer = exclude_labels_per_layer or {}
+        self.none_label = none_label
+        self.span_layer_name = span_layer_name
+        self.relation_layer_name = relation_layer_name
+        self.span_encoder_decoder_name = span_encoder_decoder_name
+        self.span_encoder_decoder_kwargs = span_encoder_decoder_kwargs
+
+        self.span_encoder_decoder: SpanEncoderDecoder
+
         self.loop_dummy_relation_name = loop_dummy_relation_name
-        self.none_label = self.id2label[none_id]
-        self.bos_id = bos_id
-        self.eos_id = eos_id
-        self.span_ids = span_ids
-        self.relation_ids = relation_ids
-        self.none_id = none_id
+        self.bos_token = bos_token
+        self.eos_token = eos_token
+
         self.ignore_error_types = ignore_error_types or []
 
+        if self.is_prepared:
+            self._post_prepare()
+
     @property
-    def target_ids(self):
-        return [self.bos_id, self.eos_id] + self.relation_ids + [self.none_id] + self.span_ids
+    def layer_names(self) -> List[str]:
+        return [self.span_layer_name, self.relation_layer_name]
+
+    @property
+    def special_targets(self) -> list[str]:
+        return [self.bos_token, self.eos_token]
+
+    @property
+    def special_target2id(self) -> Dict[str, int]:
+        return {target: idx for idx, target in enumerate(self.special_targets)}
+
+    @property
+    def target_pad_id(self) -> int:
+        return self.special_target2id[self.eos_token]
+
+    def _prepare(self, documents: Sequence[Document]):
+        span_labels: Set[str] = set()
+        relation_labels: Set[str] = set()
+        for doc in documents:
+            span_labels.update(
+                ac.label
+                for ac in doc[self.span_layer_name]
+                if ac.label not in self.exclude_labels_per_layer.get(self.span_layer_name, [])
+            )
+            relation_labels.update(
+                rel.label
+                for rel in doc[self.relation_layer_name]
+                if rel.label not in self.exclude_labels_per_layer.get(self.relation_layer_name, [])
+            )
+        self.labels_per_layer = {
+            self.span_layer_name: sorted(span_labels),
+            self.relation_layer_name: sorted(relation_labels),
+        }
+
+    def _post_prepare(self) -> None:
+        # set up labels
+        if self.labels_per_layer is None:
+            raise Exception("labels_per_layer is not defined. Call prepare() first or pass it in.")
+        self.labels: List[str] = [self.none_label]
+        for layer_name in self.layer_names:
+            self.labels.extend(self.labels_per_layer[layer_name])
+        if len(set(self.labels)) != len(self.labels):
+            raise Exception(f"labels are not unique: {self.labels}")
+
+        # set up targets and ids
+        self.targets: List[str] = self.special_targets + self.labels
+        self.target2id: Dict[str, int] = {target: idx for idx, target in enumerate(self.targets)}
+
+        # generic ids
+        self.eos_id: int = self.target2id[self.eos_token]
+        self.bos_id: int = self.target2id[self.bos_token]
+
+        # span and relation ids
+        self.span_ids: List[int] = [
+            self.target2id[label] for label in self.labels_per_layer[self.span_layer_name]
+        ]
+        self.relation_ids: List[int] = [
+            self.target2id[label] for label in self.labels_per_layer[self.relation_layer_name]
+        ]
+        # the none id is used for the dummy relation which models out-of-relation spans
+        self.none_id: int = self.target2id[self.none_label]
+
+        # span encoder decoder
+        if self.span_encoder_decoder_name == "span_encoder_decoder_with_offset":
+            self.span_encoder_decoder = SpanEncoderDecoderWithOffset(
+                offset=self.pointer_offset, **(self.span_encoder_decoder_kwargs or {})
+            )
+        elif self.span_encoder_decoder_name == "simple":
+            self.span_encoder_decoder = SimpleSpanEncoderDecoder(
+                **(self.span_encoder_decoder_kwargs or {})
+            )
+        else:
+            raise Exception(f"unknown span_encoder_decoder_name: {self.span_encoder_decoder_name}")
+
+        # helpers (same as targets / target2id, but only for labels)
+        self.label2id: Dict[str, int] = {label: self.target2id[label] for label in self.labels}
+        self.id2label: Dict[int, str] = {idx: label for label, idx in self.label2id.items()}
+        self.label_ids: List[int] = [self.label2id[label] for label in self.labels]
 
     @property
     def pointer_offset(self) -> int:
-        return len(self.target_ids)
+        return len(self.targets)
+
+    @property
+    def target_ids(self) -> Set[int]:
+        return set(range(self.pointer_offset))
 
     def sanitize_sequence(
         self,
         tag_seq: List[int],
-        target_ids: List[int],
-        span_ids: List[int],
-        relation_ids: List[int],
-        none_id: int,
     ) -> Tuple[List[Tuple[int, int, int, int, int, int, int]], Dict[str, int]]:
         # TODO: count total amounts instead of returning bool values.
         #  This requires to also count "total" (maybe also "skipped" and "correct").
@@ -157,17 +250,17 @@ class PointerNetworkSpanAndRelationEncoderDecoder(PointerNetworkEncoderDecoder):
         cur_pair: List[int] = []
         if len(tag_seq):
             for i in tag_seq:
-                if i in relation_ids or (i == none_id and len(cur_pair) == 6):
+                if i in self.relation_ids or (i == self.none_id and len(cur_pair) == 6):
                     cur_pair.append(i)
                     if len(cur_pair) != 7:
                         skip = True
                         invalid["len"] = 1
-                    elif none_id in cur_pair:
+                    elif self.none_id in cur_pair:
                         # tag = set([cur_pair[2], cur_pair[5], cur_pair[6]])
                         if not (
-                            cur_pair[2] in target_ids
-                            and cur_pair[5] in target_ids
-                            and cur_pair[6] in target_ids
+                            cur_pair[2] in self.target_ids
+                            and cur_pair[5] in self.target_ids
+                            and cur_pair[6] in self.target_ids
                         ):
                             # if not tag.issubset(add_token):
                             skip = True
@@ -183,9 +276,9 @@ class PointerNetworkSpanAndRelationEncoderDecoder(PointerNetworkEncoderDecoder):
                             skip = True
                             invalid["cover"] = 1
                         if (
-                            cur_pair[2] in relation_ids
-                            or cur_pair[5] in relation_ids
-                            or cur_pair[6] in span_ids
+                            cur_pair[2] in self.relation_ids
+                            or cur_pair[5] in self.relation_ids
+                            or cur_pair[6] in self.span_ids
                         ):
                             # Consider making an additional layer of restrictions to prevent misalignment
                             # of the relationship and span tags (可以考虑做多一层限制，防止relation 和 span标签错位)
@@ -193,7 +286,7 @@ class PointerNetworkSpanAndRelationEncoderDecoder(PointerNetworkEncoderDecoder):
                                 skip = True
                             invalid["cross"] = 1
                         # tag = set([cur_pair[2], cur_pair[5], cur_pair[6]])
-                        RC_idx = relation_ids + span_ids
+                        RC_idx = self.relation_ids + self.span_ids
                         if not (
                             cur_pair[2] in RC_idx
                             and cur_pair[5] in RC_idx
@@ -247,6 +340,13 @@ class PointerNetworkSpanAndRelationEncoderDecoder(PointerNetworkEncoderDecoder):
     def encode_relation(
         self, rel: BinaryRelation, metadata: Optional[Dict[str, Any]] = None
     ) -> Optional[List[int]]:
+        if self.labels_per_layer is None:
+            raise Exception("labels_per_layer is not defined. Call prepare() first or pass it in.")
+        if rel.label not in self.labels_per_layer[self.relation_layer_name] + [
+            self.loop_dummy_relation_name
+        ]:
+            return None
+
         encoded_head = self.encode_labeled_span(labeled_span=rel.head, metadata=metadata)
         encoded_tail = self.encode_labeled_span(labeled_span=rel.tail, metadata=metadata)
 
@@ -258,7 +358,11 @@ class PointerNetworkSpanAndRelationEncoderDecoder(PointerNetworkEncoderDecoder):
             return None
 
         if rel.label == self.loop_dummy_relation_name:
-            assert encoded_head == encoded_tail
+            if encoded_head != encoded_tail:
+                raise Exception(
+                    f"expected encoded_head == encoded_tail for loop_dummy_relation, but got: {encoded_head}, "
+                    f"{encoded_tail}"
+                )
             none_id = self.label2id[self.none_label]
             target_span = encoded_head + [none_id, none_id, none_id, none_id]
         else:
@@ -286,34 +390,42 @@ class PointerNetworkSpanAndRelationEncoderDecoder(PointerNetworkEncoderDecoder):
     ) -> List[int]:
         if not set(layers.keys()) == set(self.layer_names):
             raise Exception(f"unexpected layers: {layers.keys()}. expected: {self.layer_names}")
-        spans = layers[self.layer_names[0]]
-        relations = layers[self.layer_names[1]]
 
-        all_relation_arguments = set(chain(*[(rel.head, rel.tail) for rel in relations]))
-        dummy_loop_relations = [
-            BinaryRelation(head=span, tail=span, label=self.loop_dummy_relation_name)
-            for span in spans
-            if span not in all_relation_arguments
+        # encode relations
+        all_relation_arguments = set()
+        relation_encodings = dict()
+        for rel in layers[self.relation_layer_name]:
+            if not isinstance(rel, BinaryRelation):
+                raise Exception(f"expected BinaryRelation, but got: {rel}")
+            encoded_relation = self.encode_relation(rel=rel, metadata=metadata)
+            if encoded_relation is not None:
+                relation_encodings[rel] = encoded_relation
+                all_relation_arguments.update([rel.head, rel.tail])
+
+        # encode spans that are not arguments of any relation
+        no_relation_spans = [
+            span for span in layers[self.span_layer_name] if span not in all_relation_arguments
         ]
-        relations_with_dummies = list(relations) + dummy_loop_relations
+        for span in no_relation_spans:
+            dummy_relation = BinaryRelation(
+                head=span, tail=span, label=self.loop_dummy_relation_name
+            )
+            encoded_relation = self.encode_relation(rel=dummy_relation, metadata=metadata)
+            if encoded_relation is not None:
+                relation_encodings[dummy_relation] = encoded_relation
 
-        sorted_relations = sorted(relations_with_dummies, key=cmp_to_key(cmp_src_rel))
+        # sort relations by start indices of head and tail
+        sorted_relations = sorted(relation_encodings, key=cmp_to_key(cmp_src_rel))
 
+        # build target tokens
         tgt_tokens = [self.bos_id]
         for rel in sorted_relations:
-            new_target_span = self.encode_relation(rel=rel, metadata=metadata)
-            if new_target_span is not None:
-                tgt_tokens.extend(new_target_span)
+            encoded_relation = relation_encodings[rel]
+            tgt_tokens.extend(encoded_relation)
         tgt_tokens.append(self.eos_id)
 
         # sanity check
-        _, invalid = self.sanitize_sequence(
-            tag_seq=tgt_tokens[1:],
-            target_ids=self.target_ids,
-            span_ids=self.span_ids,
-            relation_ids=self.relation_ids,
-            none_id=self.none_id,
-        )
+        _, invalid = self.sanitize_sequence(tag_seq=tgt_tokens[1:])
         if not all(v == 0 for k, v in invalid.items() if k not in self.ignore_error_types):
             decoded, invalid = self.decode(tgt_tokens, metadata=metadata)
             logger.warning(f"invalid: {invalid}, decoded: {decoded}")
@@ -323,14 +435,8 @@ class PointerNetworkSpanAndRelationEncoderDecoder(PointerNetworkEncoderDecoder):
     def decode(
         self, targets: List[int], metadata: Optional[Dict[str, Any]] = None
     ) -> Tuple[Dict[str, List[Annotation]], Any]:
-        ps, _errors = self.sanitize_sequence(
-            # strip the bos token
-            tag_seq=targets[1:],
-            target_ids=self.target_ids,
-            span_ids=self.span_ids,
-            relation_ids=self.relation_ids,
-            none_id=self.none_id,
-        )
+        # strip the bos token
+        ps, _errors = self.sanitize_sequence(tag_seq=targets[1:])
         relation_tuples: List[Tuple[Tuple[int, int], Tuple[int, int], str]] = []
         entity_labels: Dict[Tuple[int, int], List[str]] = defaultdict(list)
         for tup in ps:
