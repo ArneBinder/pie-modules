3c31e6c3df4212969242b9a07cc7aec1b06bd259
Arne Binder <arne.binder@dfki.de>
2023-12-09 19:10:36 +0100

    minor changes in documents / annotations

--- src/pie_modules/annotations.py
+++ src/pie_modules/annotations.py
@@ -19,14 +19,15 @@ class Question(Annotation):
 class ExtractiveAnswer(Span):
     """An answer to a question."""
 
-    TARGET_NAMES = ["base", "questions"]
+    # this annotation has two target fields
+    TARGET_NAMES = ("base", "questions")
 
     question: Question
     # The score of the answer. This is not considered when comparing two answers (e.g. prediction with gold).
     score: Optional[float] = dataclasses.field(default=None, compare=False)
 
     def __str__(self) -> str:
-        if self.targets is None:
+        if not self.is_attached:
             return ""
         # we assume that the first target is the text
         context = self.named_targets["base"]
--- src/pie_modules/documents.py
+++ src/pie_modules/documents.py
@@ -8,12 +8,12 @@ from pie_modules.annotations import ExtractiveAnswer, Question
 
 @dataclasses.dataclass
 class ExtractiveQADocument(TextBasedDocument):
-    """A PIE document with annotations for extractive question answering."""
+    """A text based PIE document with annotations for extractive question answering."""
 
     questions: AnnotationList[Question] = annotation_field()
     # Note: We define the target fields / layers of the answers layer in the following way. Any answer
     # targets the text field and (one entry of) the questions layer, so named_targets needs to contain
-    # these keys. See ExtractiveAnswer.TARGET_NAMES for the required *values* for named_targets.
+    # these *values*. See ExtractiveAnswer.TARGET_NAMES for the required *keys* for named_targets.
     answers: AnnotationList[ExtractiveAnswer] = annotation_field(
         named_targets={"base": "text", "questions": "questions"}
     )
@@ -26,7 +26,7 @@ class TokenizedExtractiveQADocument(TokenBasedDocument):
     questions: AnnotationList[Question] = annotation_field()
     # Note: We define the target fields / layers of the answers layer in the following way. Any answer
     # targets the text field and (one entry of) the questions layer, so named_targets needs to contain
-    # these keys. See ExtractiveAnswer.TARGET_NAMES for the required *values* for named_targets.
+    # these *values*. See ExtractiveAnswer.TARGET_NAMES for the required *keys* for named_targets.
     answers: AnnotationList[ExtractiveAnswer] = annotation_field(
         named_targets={"base": "tokens", "questions": "questions"}
     )
