a726e41f42101069d07a9d4be0f8252b1f347f98
Arne Binder <arne.binder@dfki.de>
2024-01-04 14:08:54 +0100

    add documentation to interface and fix class name

--- src/pie_modules/models/simple_generative.py
+++ src/pie_modules/models/simple_generative.py
@@ -11,7 +11,7 @@ from torch.optim import Optimizer
 from torchmetrics import Metric
 from transformers import PreTrainedModel, get_linear_schedule_with_warmup
 
-from pie_modules.models.interface import RequiresTaskModuleConfig
+from pie_modules.models.interface import RequiresTaskmoduleConfig
 from pie_modules.utils import resolve_type
 
 logger = logging.getLogger(__name__)
@@ -36,7 +36,7 @@ def flatten_dict(d: MutableMapping, parent_key: str = "", sep: str = "."):
 
 
 @PyTorchIEModel.register()
-class SimpleGenerativeModel(PyTorchIEModel, RequiresTaskModuleConfig):
+class SimpleGenerativeModel(PyTorchIEModel, RequiresTaskmoduleConfig):
     def __init__(
         self,
         # base model setup
