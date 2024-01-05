686dff359c159c0d019058cd5f53230d64eb8283
Arne Binder <arne.binder@dfki.de>
2024-01-05 23:00:28 +0100

    increase test coverage

--- tests/models/base_models/test_bart_with_decoder_position_ids.py
+++ tests/models/base_models/test_bart_with_decoder_position_ids.py
@@ -114,7 +114,9 @@ def test_bart_decoder_with_position_ids_forward(bart_decoder_with_position_ids):
 @pytest.fixture(scope="module")
 def bart_model_with_decoder_position_ids(bart_config):
     torch.manual_seed(42)
-    return BartModelWithDecoderPositionIds(config=bart_config)
+    model = BartModelWithDecoderPositionIds(config=bart_config)
+    model.train()
+    return model
 
 
 def test_bart_model_with_decoder_position_ids(bart_model_with_decoder_position_ids):
@@ -160,29 +162,37 @@ def test_bart_model_with_decoder_position_ids_get_decoder(bart_model_with_decode
 
 
 @pytest.mark.parametrize(
-    "return_dict",
-    [True, False],
+    "return_dict, prepare_encoder_outputs, output_everything",
+    [(True, True, True), (False, False, False)],
 )
 def test_bart_model_with_decoder_position_forward(
-    bart_model_with_decoder_position_ids, return_dict
+    bart_model_with_decoder_position_ids, return_dict, prepare_encoder_outputs, output_everything
 ):
-    # Arrange
     model = bart_model_with_decoder_position_ids
+
+    # Arrange
+    model.eval()
     input_ids = torch.tensor([[0, 1, 2, 3, 4, 5, 6, 7]])
     position_ids_original = torch.tensor([[0, 1, 2, 3, 4, 5, 6, 7]])
     position_ids_different = torch.tensor([[0, 0, 0, 1, 1, 1, 2, 2]])
+    common_kwargs = {"input_ids": input_ids, "return_dict": return_dict}
+    if prepare_encoder_outputs:
+        common_kwargs["encoder_outputs"] = bart_model_with_decoder_position_ids.get_encoder()(
+            input_ids=input_ids, return_dict=False
+        )
+    else:
+        common_kwargs["encoder_outputs"] = None
+    if output_everything:
+        common_kwargs["output_attentions"] = True
+        common_kwargs["output_hidden_states"] = True
 
     # Act
-    torch.manual_seed(42)
-    original = model(input_ids=input_ids, return_dict=return_dict)[0]
-    torch.manual_seed(42)
+    original = model(**common_kwargs)[0]
     replaced_original = model(
-        input_ids=input_ids, decoder_position_ids=position_ids_original, return_dict=return_dict
+        decoder_position_ids=position_ids_original,
+        **common_kwargs,
     )[0]
-    torch.manual_seed(42)
-    replaced_different = model(
-        input_ids=input_ids, decoder_position_ids=position_ids_different, return_dict=return_dict
-    )[0]
+    replaced_different = model(decoder_position_ids=position_ids_different, **common_kwargs)[0]
 
     # Assert
     assert isinstance(original, torch.FloatTensor)
@@ -191,11 +201,11 @@ def test_bart_model_with_decoder_position_forward(
         original[0, :5, :3],
         torch.tensor(
             [
-                [0.7961970567703247, 1.2232387065887451, 0.7286717295646667],
-                [0.034051503986120224, -0.9746682047843933, -0.700711190700531],
-                [0.1363907903432846, -0.4540761113166809, -1.2949464321136475],
-                [1.1136258840560913, -0.1388537585735321, 1.538393259048462],
-                [-1.1127841472625732, 0.22768200933933258, 1.6438117027282715],
+                [0.7589594721794128, 1.0452316999435425, 0.7063764333724976],
+                [-0.12192550301551819, -0.9932114481925964, -0.722382664680481],
+                [0.24711951613426208, -0.291597843170166, -1.0466505289077759],
+                [1.1228691339492798, -0.0873560905456543, 1.534016728401184],
+                [-1.1132177114486694, 0.2277398556470871, 1.6456809043884277],
             ]
         ),
     )
@@ -204,14 +214,14 @@ def test_bart_model_with_decoder_position_forward(
         torch.tensor(
             [
                 [
-                    -2.384185791015625e-07,
-                    -4.76837158203125e-07,
+                    0.0,
+                    -1.1920928955078125e-07,
+                    -1.1920928955078125e-07,
                     -2.682209014892578e-07,
-                    2.086162567138672e-07,
                     5.960464477539063e-08,
                     5.960464477539063e-08,
-                    0.0,
-                    0.0,
+                    2.384185791015625e-07,
+                    -5.960464477539063e-08,
                 ]
             ]
         ),
@@ -225,11 +235,11 @@ def test_bart_model_with_decoder_position_forward(
         replaced_different[0, :5, :3],
         torch.tensor(
             [
-                [0.7961970567703247, 1.2232387065887451, 0.7286717295646667],
-                [0.1183161735534668, -0.7555443048477173, -1.230163812637329],
-                [1.2578136920928955, 0.18759475648403168, -0.1578090786933899],
-                [0.5176712870597839, 0.9378399848937988, 1.3435578346252441],
-                [0.6121589541435242, -1.0105386972427368, 2.361997365951538],
+                [0.7589594721794128, 1.0452316999435425, 0.7063764333724976],
+                [-0.0127173513174057, -0.8127143383026123, -1.256797194480896],
+                [1.0517312288284302, 0.037927787750959396, -0.28661563992500305],
+                [0.5884698629379272, 0.9930593371391296, 1.3842554092407227],
+                [0.6132885813713074, -1.0105736255645752, 2.361264228820801],
             ]
         ),
     )
@@ -238,14 +248,14 @@ def test_bart_model_with_decoder_position_forward(
         torch.tensor(
             [
                 [
+                    0.0,
                     -2.384185791015625e-07,
-                    -4.76837158203125e-07,
-                    -2.682209014892578e-07,
-                    2.086162567138672e-07,
-                    5.960464477539063e-08,
-                    5.960464477539063e-08,
-                    0.0,
-                    0.0,
+                    -1.7881393432617188e-07,
+                    2.5331974029541016e-07,
+                    1.4901161193847656e-07,
+                    1.1920928955078125e-07,
+                    -1.1920928955078125e-07,
+                    -1.7881393432617188e-07,
                 ]
             ]
         ),
