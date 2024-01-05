1de31965dc0cf9c6b7c513412280aa0631d1dbda
Arne Binder <arne.binder@dfki.de>
2024-01-05 22:00:44 +0100

    add test_bart_decoder_with_position_ids_forward()

--- tests/models/base_models/test_bart_with_decoder_position_ids.py
+++ tests/models/base_models/test_bart_with_decoder_position_ids.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 from torch.nn import Embedding
 from transformers import BartConfig
+from transformers.modeling_outputs import BaseModelOutputWithPastAndCrossAttentions
 from transformers.models.bart.modeling_bart import BartLearnedPositionalEmbedding
 
 from pie_modules.models.base_models.bart_with_decoder_position_ids import (
@@ -75,3 +76,29 @@ def test_bart_decoder_with_position_ids_set_input_embeddings(bart_decoder_with_p
     assert input_embeddings is not original_input_embeddings
     # recover original input embeddings
     bart_decoder_with_position_ids.set_input_embeddings(original_input_embeddings)
+
+
+def test_bart_decoder_with_position_ids_forward(bart_decoder_with_position_ids):
+    # Arrange
+    model = bart_decoder_with_position_ids
+    input_ids = torch.tensor([[0, 1, 2, 3, 4, 5, 6, 7]])
+    position_ids_original = torch.tensor([[0, 1, 2, 3, 4, 5, 6, 7]])
+    position_ids_different = torch.tensor([[0, 0, 0, 1, 1, 1, 2, 2]])
+
+    # Act
+    torch.manual_seed(42)
+    original = model(input_ids=input_ids)
+    torch.manual_seed(42)
+    replaced_original = model(input_ids=input_ids, position_ids=position_ids_original)
+    torch.manual_seed(42)
+    replaced_different = model(input_ids=input_ids, position_ids=position_ids_different)
+
+    # Assert
+    assert isinstance(original, BaseModelOutputWithPastAndCrossAttentions)
+    assert original.last_hidden_state.shape == (1, 8, 10)
+    assert isinstance(replaced_original, BaseModelOutputWithPastAndCrossAttentions)
+    torch.testing.assert_close(original.last_hidden_state, replaced_original.last_hidden_state)
+
+    assert isinstance(replaced_different, BaseModelOutputWithPastAndCrossAttentions)
+    assert replaced_different.last_hidden_state.shape == (1, 8, 10)
+    assert not torch.allclose(original.last_hidden_state, replaced_different.last_hidden_state)
