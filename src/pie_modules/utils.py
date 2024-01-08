ad94a2ed753a4bc58cffbdbd623fd1e87555ad52
Arne Binder <arne.binder@dfki.de>
2024-01-08 13:07:18 +0100

    revert change in utils.py

--- src/pie_modules/utils.py
+++ src/pie_modules/utils.py
@@ -10,15 +10,6 @@ T_super = TypeVar("T_super", bound=Document)
 def resolve_type(
     type_or_str: Union[str, Type[T]], expected_super_type: Optional[Type[T_super]] = None
 ) -> Type[T]:
-    """Resolve a type from a string or return the type itself if it is already a type.
-
-    Args:
-        type_or_str: The type or a string that resolves to the type.
-        expected_super_type: An optional type that the resolved type must be a subclass of.
-
-    Returns: The resolved type.
-    """
-
     if isinstance(type_or_str, str):
         dt = resolve_target(type_or_str)  # type: ignore
     else:
