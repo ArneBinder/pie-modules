make PrecisionRecallAndF1ForLabeledAnnotations a real Metric (fix state) and add macro average

src/pie_modules/taskmodules/metrics/precision_recall_and_f1_for_labeled_annotations.py
@@ -1,9 +1,10 @@
 import logging
 from collections import Counter
-from typing import Any, Dict, Iterable, Optional, Sequence, Tuple, Union
+from typing import Any, Collection, Dict, Iterable, Optional, Union
 
 import torch
 from pytorch_ie import Annotation
+from torch import LongTensor
 from torchmetrics import Metric
 
 from pie_modules.utils import flatten_nested_dict
@@ -29,34 +30,49 @@ class PrecisionRecallAndF1ForLabeledAnnotations(Metric):
     def __init__(
         self,
         label_mapping: Optional[Dict[Any, str]] = None,
-        key_micro: str = "micro",
+        key_micro: Optional[str] = "micro",
+        key_macro: Optional[str] = "macro",
         in_percent: bool = False,
         flatten_result_with_sep: Optional[str] = None,
     ):
         super().__init__()
         self.label_mapping = label_mapping
         self.key_micro = key_micro
+        self.key_macro = key_macro
         self.in_percent = in_percent
         self.flatten_result_with_sep = flatten_result_with_sep
-        self.add_state("gold", default=[])
-        self.add_state("predicted", default=[])
-        self.add_state("correct", default=[])
-        self.add_state("idx", default=torch.tensor(0))
+
+    def inc_counts(self, counts: LongTensor, key: Optional[str], prefix: str = "counts_"):
+        full_key = prefix
+        if key is not None:
+            full_key += key
+
+        if not hasattr(self, full_key):
+            self.add_state(full_key, default=torch.zeros_like(counts), dist_reduce_fx="sum")
+
+        prev_value = getattr(self, full_key)
+        setattr(self, full_key, prev_value + counts)
+
+    def get_counts(self, key_prefix: str = "counts_") -> Dict[Optional[str], LongTensor]:
+        result = {}
+        for k, v in self.metric_state.items():
+            if k.startswith(key_prefix):
+                key = k[len(key_prefix) :] or None
+                result[key] = v
+        return result
 
     def update(self, gold: Iterable[Annotation], predicted: Iterable[Annotation]) -> None:
-        # remove duplicates within each list, but collect them with the instance idx to allow
-        # for same annotations in different examples (otherwise they would be counted as one
-        # because they are not attached to a specific document)
-        gold_set = {(self.idx.item(), ann) for ann in set(gold)}
-        predicted_set = {(self.idx.item(), ann) for ann in set(predicted)}
-        self.gold.extend(gold_set)
-        self.predicted.extend(predicted_set)
-        self.correct.extend(gold_set & predicted_set)
-        self.idx += 1
+        # remove duplicates within each list
+        gold_set = set(gold)
+        predicted_set = set(predicted)
+        new_counts = self.calculate_counts(gold_set, predicted_set, gold_set & predicted_set)
+        for k, v in new_counts.items():
+            self.inc_counts(counts=v, key=k)
 
-    def get_precision_recall_f1(
-        self, n_gold: int, n_predicted: int, n_correct: int
-    ) -> Dict[str, float]:
+    def get_precision_recall_f1(self, n_gold_predicted_correct: LongTensor) -> Dict[str, float]:
+        n_gold = n_gold_predicted_correct[0]
+        n_predicted = n_gold_predicted_correct[1]
+        n_correct = n_gold_predicted_correct[2]
         recall = 0.0 if n_gold == 0 else (n_correct / n_gold)
         precision = 0.0 if n_predicted == 0 else (n_correct / n_predicted)
         f1 = 0.0 if recall + precision == 0 else (2 * precision * recall) / (precision + recall)
@@ -65,7 +81,11 @@ class PrecisionRecallAndF1ForLabeledAnnotations(Metric):
             recall *= 100
             precision *= 100
             f1 *= 100
-        return {"recall": recall, "precision": precision, "f1": f1}
+        return {
+            "recall": torch.tensor(recall).to(self.device),
+            "precision": torch.tensor(precision).to(self.device),
+            "f1": torch.tensor(f1).to(self.device),
+        }
 
     def get_label(self, annotation: Annotation) -> Optional[str]:
         label: Optional[str] = getattr(annotation, "label", None)
@@ -73,36 +93,50 @@ class PrecisionRecallAndF1ForLabeledAnnotations(Metric):
             return self.label_mapping[label]
         return label
 
-    def get_counts(
+    def calculate_counts(
         self,
-        gold: Sequence[Annotation],
-        predicted: Sequence[Annotation],
-        correct: Sequence[Annotation],
-    ) -> Dict[Optional[str], Tuple[int, int, int]]:
+        gold: Collection[Annotation],
+        predicted: Collection[Annotation],
+        correct: Collection[Annotation],
+    ) -> Dict[Optional[str], LongTensor]:
         result = {}
         # per class
-        gold_counter = Counter([self.get_label(ann) for idx, ann in gold])
-        predicted_counter = Counter([self.get_label(ann) for idx, ann in predicted])
-        correct_counter = Counter([self.get_label(ann) for idx, ann in correct])
+        gold_counter = Counter([self.get_label(ann) for ann in gold])
+        predicted_counter = Counter([self.get_label(ann) for ann in predicted])
+        correct_counter = Counter([self.get_label(ann) for ann in correct])
         for label in gold_counter.keys() | predicted_counter.keys():
-            if label == self.key_micro:
+            if self.key_micro is not None and label == self.key_micro:
                 raise ValueError(
                     f"The key '{self.key_micro}' was used as an annotation label, but it is reserved for "
                     f"the micro average. You can change which key is used for that with the 'key_micro' argument."
                 )
-            result[label] = (
-                gold_counter.get(label, 0),
-                predicted_counter.get(label, 0),
-                correct_counter.get(label, 0),
-            )
+            result[label] = torch.tensor(
+                [
+                    gold_counter.get(label, 0),
+                    predicted_counter.get(label, 0),
+                    correct_counter.get(label, 0),
+                ]
+            ).to(device=self.device)
 
         # overall
-        result[self.key_micro] = (len(gold), len(predicted), len(correct))
+        if self.key_micro is not None:
+            result[self.key_micro] = torch.tensor([len(gold), len(predicted), len(correct)]).to(
+                device=self.device
+            )
         return result
 
     def compute(self) -> Union[Dict[str, Any], Dict[Optional[str], dict[str, float]]]:
-        counts = self.get_counts(self.gold, self.predicted, self.correct)
-        result = {label: self.get_precision_recall_f1(*counts[label]) for label in counts.keys()}
+        counts = self.get_counts()
+        result = {label: self.get_precision_recall_f1(counts[label]) for label in counts.keys()}
+        if self.key_macro is not None:
+            result_without_micro = {
+                k: v for k, v in result.items() if self.key_micro is None or k != self.key_micro
+            }
+            if len(result_without_micro) > 0:
+                result[self.key_macro] = {
+                    k: torch.stack([v[k] for v in result_without_micro.values()]).mean()
+                    for k in ["precision", "recall", "f1"]
+                }
 
         if self.flatten_result_with_sep is not None:
             return flatten_nested_dict(result, sep=self.flatten_result_with_sep)

tests/taskmodules/metrics/test_precision_recall_and_f1_for_labeled_annotations.py
@@ -1,32 +1,24 @@
 import pytest
-import torch
 from pytorch_ie.annotations import LabeledSpan
+from torch import tensor
 
 from pie_modules.taskmodules.metrics import PrecisionRecallAndF1ForLabeledAnnotations
 
 
 def test_precision_recall_and_f1_for_labeled_annotations():
     metric = PrecisionRecallAndF1ForLabeledAnnotations()
-    assert metric.compute() == {"micro": {"f1": 0.0, "precision": 0.0, "recall": 0.0}}
-    assert metric.metric_state == {
-        "gold": [],
-        "predicted": [],
-        "correct": [],
-        "idx": torch.tensor(0),
-    }
+    assert metric.metric_state == {}
 
     metric.update(
         gold=[LabeledSpan(start=0, end=1, label="a")],
         predicted=[LabeledSpan(start=0, end=1, label="a")],
     )
-    assert metric.metric_state == {
-        "gold": [(0, LabeledSpan(start=0, end=1, label="a", score=1.0))],
-        "predicted": [(0, LabeledSpan(start=0, end=1, label="a", score=1.0))],
-        "correct": [(0, LabeledSpan(start=0, end=1, label="a", score=1.0))],
-        "idx": torch.tensor(1),
-    }
-    assert metric.compute() == {
+    metric_state = {k: v.tolist() for k, v in metric.metric_state.items()}
+    assert metric_state == {"counts_a": [1, 1, 1], "counts_micro": [1, 1, 1]}
+    value = metric.compute()
+    assert value == {
         "a": {"recall": 1.0, "precision": 1.0, "f1": 1.0},
+        "macro": {"recall": 1.0, "precision": 1.0, "f1": 1.0},
         "micro": {"recall": 1.0, "precision": 1.0, "f1": 1.0},
     }
 
@@ -35,23 +27,22 @@ def test_precision_recall_and_f1_for_labeled_annotations():
         gold=[LabeledSpan(start=0, end=1, label="a"), LabeledSpan(start=0, end=1, label="b")],
         predicted=[LabeledSpan(start=0, end=1, label="b"), LabeledSpan(start=0, end=1, label="c")],
     )
-    assert set(metric.metric_state) == {"gold", "predicted", "correct", "idx"}
-    assert metric.metric_state["idx"] == torch.tensor(1)
-    assert set(metric.metric_state["gold"]) == {
-        (0, LabeledSpan(start=0, end=1, label="a", score=1.0)),
-        (0, LabeledSpan(start=0, end=1, label="b", score=1.0)),
-    }
-    assert set(metric.metric_state["predicted"]) == {
-        (0, LabeledSpan(start=0, end=1, label="b", score=1.0)),
-        (0, LabeledSpan(start=0, end=1, label="c", score=1.0)),
-    }
-    assert set(metric.metric_state["correct"]) == {
-        (0, LabeledSpan(start=0, end=1, label="b", score=1.0)),
+    metric_state = {k: v.tolist() for k, v in metric.metric_state.items()}
+    assert metric_state == {
+        "counts_a": [1, 0, 0],
+        "counts_b": [1, 1, 1],
+        "counts_c": [0, 1, 0],
+        "counts_micro": [2, 2, 1],
     }
     assert metric.compute() == {
         "b": {"recall": 1.0, "precision": 1.0, "f1": 1.0},
         "a": {"recall": 0.0, "precision": 0.0, "f1": 0.0},
         "c": {"recall": 0.0, "precision": 0.0, "f1": 0.0},
+        "macro": {
+            "f1": tensor(0.3333333432674408),
+            "precision": tensor(0.3333333432674408),
+            "recall": tensor(0.3333333432674408),
+        },
         "micro": {"recall": 0.5, "precision": 0.5, "f1": 0.5},
     }
 
@@ -69,24 +60,12 @@ def test_precision_recall_and_f1_for_labeled_annotations():
             LabeledSpan(start=0, end=1, label="c"),
         ],
     )
-    assert set(metric.metric_state) == {"gold", "predicted", "correct", "idx"}
-    assert metric.metric_state["idx"] == torch.tensor(1)
-    assert set(metric.metric_state["gold"]) == {
-        (0, LabeledSpan(start=0, end=1, label="a", score=1.0)),
-        (0, LabeledSpan(start=0, end=1, label="b", score=1.0)),
-    }
-    assert set(metric.metric_state["predicted"]) == {
-        (0, LabeledSpan(start=0, end=1, label="b", score=1.0)),
-        (0, LabeledSpan(start=0, end=1, label="c", score=1.0)),
-    }
-    assert set(metric.metric_state["correct"]) == {
-        (0, LabeledSpan(start=0, end=1, label="b", score=1.0)),
-    }
-    assert metric.compute() == {
-        "b": {"recall": 1.0, "precision": 1.0, "f1": 1.0},
-        "a": {"recall": 0.0, "precision": 0.0, "f1": 0.0},
-        "c": {"recall": 0.0, "precision": 0.0, "f1": 0.0},
-        "micro": {"recall": 0.5, "precision": 0.5, "f1": 0.5},
+    metric_state = {k: v.tolist() for k, v in metric.metric_state.items()}
+    assert metric_state == {
+        "counts_a": [1, 0, 0],
+        "counts_b": [1, 1, 1],
+        "counts_c": [0, 1, 0],
+        "counts_micro": [2, 2, 1],
     }
 
     # assert no deduplication over multiple updates
@@ -99,35 +78,45 @@ def test_precision_recall_and_f1_for_labeled_annotations():
         gold=[LabeledSpan(start=0, end=1, label="b")],
         predicted=[LabeledSpan(start=0, end=1, label="a")],
     )
-    assert set(metric.metric_state) == {"gold", "predicted", "correct", "idx"}
-    assert metric.metric_state["idx"] == torch.tensor(2)
-    assert set(metric.metric_state["gold"]) == {
-        (0, LabeledSpan(start=0, end=1, label="a", score=1.0)),
-        (1, LabeledSpan(start=0, end=1, label="b", score=1.0)),
+    metric_state = {k: v.tolist() for k, v in metric.metric_state.items()}
+    assert metric_state == {
+        "counts_a": [1, 1, 0],
+        "counts_b": [1, 1, 0],
+        "counts_c": [0, 0, 0],
+        "counts_micro": [2, 2, 0],
     }
-    assert set(metric.metric_state["predicted"]) == {
-        (0, LabeledSpan(start=0, end=1, label="b", score=1.0)),
-        (1, LabeledSpan(start=0, end=1, label="a", score=1.0)),
-    }
-    assert set(metric.metric_state["correct"]) == set()
     assert metric.compute() == {
         "a": {"f1": 0.0, "precision": 0.0, "recall": 0.0},
         "b": {"f1": 0.0, "precision": 0.0, "recall": 0.0},
+        "c": {"f1": 0.0, "precision": 0.0, "recall": 0.0},
+        "macro": {"f1": 0.0, "precision": 0.0, "recall": 0.0},
         "micro": {"f1": 0.0, "precision": 0.0, "recall": 0.0},
     }
 
 
 def test_precision_recall_and_f1_for_labeled_annotations_in_percent():
-    metric = PrecisionRecallAndF1ForLabeledAnnotations(in_percent=True)
+    metric = PrecisionRecallAndF1ForLabeledAnnotations(
+        in_percent=True, flatten_result_with_sep="/"
+    )
 
     metric.update(
         gold=[LabeledSpan(start=0, end=1, label="a")],
         predicted=[LabeledSpan(start=0, end=1, label="a"), LabeledSpan(start=0, end=1, label="b")],
     )
-    assert metric.compute() == {
-        "b": {"recall": 0.0, "precision": 0.0, "f1": 0.0},
-        "a": {"recall": 100.0, "precision": 100.0, "f1": 100.0},
-        "micro": {"recall": 100.0, "precision": 50.0, "f1": 66.66666666666666},
+    values = {k: v.item() for k, v in metric.compute().items()}
+    assert values == {
+        "a/f1": 100.0,
+        "a/precision": 100.0,
+        "a/recall": 100.0,
+        "b/f1": 0.0,
+        "b/precision": 0.0,
+        "b/recall": 0.0,
+        "macro/f1": 50.0,
+        "macro/precision": 50.0,
+        "macro/recall": 50.0,
+        "micro/f1": 66.66667175292969,
+        "micro/precision": 50.0,
+        "micro/recall": 100.0,
     }
 
 
@@ -143,18 +132,18 @@ def test_precision_recall_and_f1_for_labeled_annotations_with_label_mapping():
     assert metric.compute() == {
         "label_a": {"f1": 1.0, "precision": 1.0, "recall": 1.0},
         "label_b": {"f1": 0.0, "precision": 0.0, "recall": 0.0},
+        "macro": {"f1": 0.5, "precision": 0.5, "recall": 0.5},
         "micro": {"f1": 0.6666666666666666, "precision": 0.5, "recall": 1.0},
     }
 
 
 def test_precision_recall_and_f1_for_labeled_annotations_key_micro_error():
     metric = PrecisionRecallAndF1ForLabeledAnnotations()
-    metric.update(
-        gold=[LabeledSpan(start=0, end=1, label="micro")],
-        predicted=[],
-    )
     with pytest.raises(ValueError) as excinfo:
-        metric.compute()
+        metric.update(
+            gold=[LabeledSpan(start=0, end=1, label="micro")],
+            predicted=[],
+        )
     assert (
         str(excinfo.value)
         == "The key 'micro' was used as an annotation label, but it is reserved for the micro average. "

tests/taskmodules/test_token_classification.py
@@ -731,12 +731,7 @@ def test_configure_model_metric(documents):
 
     metric = taskmodule.configure_model_metric(stage="test")
     values = metric.compute()
-    assert values == {
-        "metric/micro/f1/test": 0.0,
-        "metric/micro/precision/test": 0.0,
-        "metric/micro/recall/test": 0.0,
-        "metric/token/macro/f1/test": torch.tensor(0.0),
-    }
+    assert values == {"metric/token/macro/f1/test": torch.tensor(0.0)}
 
     batch = taskmodule.collate(taskmodule.encode(documents, encode_target=True))
     targets = batch[1]