4186a73d3784e36c53158953e914557d89c76237
Arne Binder <arne.binder@dfki.de>
2024-01-18 19:46:07 +0100

    add documentation to SimpleTokenClassificationModel

--- src/pie_modules/models/simple_token_classification.py
+++ src/pie_modules/models/simple_token_classification.py
@@ -30,6 +30,25 @@ class SimpleTokenClassificationModel(
     RequiresModelNameOrPath,
     RequiresNumClasses,
 ):
+    """A simple token classification model that wraps a (pretrained) model loaded with
+    AutoModelForTokenClassification from the transformers library.
+
+    The model is trained with a cross-entropy loss function and uses the Adam optimizer.
+
+    Note that for training, the labels for the special tokens (as well as for padding tokens)
+    are expected to have the value label_pad_id (-100 by default, which is the default ignore_index
+    value for the CrossEntropyLoss). The predictions for these tokens are also replaced with
+    label_pad_id to match the training labels for correct metric calculation. Therefore, the model
+    requires the special_tokens_mask and attention_mask (for padding) to be passed as inputs.
+
+    Args:
+        model_name_or_path: The name or path of the pretrained transformer model to use.
+        num_classes: The number of classes to predict.
+        learning_rate: The learning rate to use for training.
+        label_pad_id: The label id to use for padding labels (at the padding token positions
+            as well as for the special tokens).
+    """
+
     def __init__(
         self,
         model_name_or_path: str,
@@ -59,7 +78,7 @@ class SimpleTokenClassificationModel(
         }
         return self.model(labels=targets, **inputs_without_special_tokens_mask)
 
-    def decode(self, inputs: InputType, outputs: InputType) -> TargetType:
+    def decode(self, inputs: InputType, outputs: OutputType) -> TargetType:
         # get the max index for each token from the logits
         tags_tensor = torch.argmax(outputs.logits, dim=-1).to(torch.long)
 
