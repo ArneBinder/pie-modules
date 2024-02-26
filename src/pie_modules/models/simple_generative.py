7f02e9c9e6a18876e8b92f5a932c675190225693
Arne Binder <arne.binder@dfki.de>
2024-02-26 21:44:59 +0100

    add Exponential Moving Average (EMA) support to SimpleGenerativeModel

--- src/pie_modules/models/simple_generative.py
+++ src/pie_modules/models/simple_generative.py
@@ -3,6 +3,7 @@ import logging
 from typing import Any, Dict, Optional, Tuple, Type, Union
 
 import torch
+from ema_pytorch import EMA
 from pytorch_ie.core import PyTorchIEModel
 from pytorch_lightning.utilities.types import OptimizerLRScheduler
 from torch import FloatTensor, LongTensor
@@ -11,7 +12,7 @@ from transformers import PreTrainedModel, get_linear_schedule_with_warmup
 from transformers.modeling_outputs import Seq2SeqLMOutput
 from typing_extensions import TypeAlias
 
-from pie_modules.models.common import ModelWithBoilerplate
+from pie_modules.models.common import TRAINING, ModelWithBoilerplate
 from pie_modules.utils import resolve_type
 
 logger = logging.getLogger(__name__)
@@ -65,6 +66,8 @@ class SimpleGenerativeModel(
         override_generation_kwargs: Optional[Dict[str, Any]] = None,
         # scheduler / optimizer
         warmup_proportion: float = 0.0,
+        # Exponential Moving Average (EMA)
+        use_ema: bool = False,
         # important: the following entries are only used if the base model does not have a configure_optimizer method!
         learning_rate: Optional[float] = None,
         optimizer_type: Optional[Union[str, Type[Optimizer]]] = None,
@@ -81,8 +84,20 @@ class SimpleGenerativeModel(
         # Note: We do not set expected_super_type=PreTrainedModel for resolve_type() because
         #   AutoModel* classed such as AutoModelForSeq2SeqLM do not inherit from that.
         resolved_base_model_type: Type[PreTrainedModel] = resolve_type(base_model_type)
-        self.model = resolved_base_model_type.from_pretrained(**base_model_config)
+        self._model = resolved_base_model_type.from_pretrained(**base_model_config)
         self.generation_config = self.configure_generation(**(override_generation_kwargs or {}))
+        self.use_ema = use_ema
+        if use_ema:
+            self._model = EMA(
+                self._model,
+                beta=0.9999,  # exponential moving average factor
+                update_after_step=100,  # only after this number of .update() calls will it start updating
+                update_every=10,  # how often to actually update, to save on compute (updates every 10th .update() call)
+            )
+
+    @property
+    def model(self) -> PreTrainedModel:
+        return self._model if not self.use_ema else self._model.model
 
     def configure_generation(self, **kwargs) -> Dict[str, Any]:
         if self.taskmodule is not None:
@@ -96,6 +111,12 @@ class SimpleGenerativeModel(
             generation_config = {}
         generation_config.update(kwargs)
         return generation_config
+
+    def training_step(self, batch: StepInputType, batch_idx: int) -> StepOutputType:
+        result = self._step(stage=TRAINING, batch=batch)
+        if self.use_ema:
+            self._model.update()
+        return result
 
     def predict(self, inputs, **kwargs) -> TargetType:
         is_training = self.training
@@ -117,7 +138,7 @@ class SimpleGenerativeModel(
 
     def forward(self, inputs: InputType, targets: Optional[TargetType] = None) -> OutputType:
         kwargs = {**inputs, **(targets or {})}
-        return self.model(**kwargs)
+        return self._model(**kwargs)
 
     def decode(self, inputs: InputType, outputs: OutputType) -> TargetType:
         # construct prediction from the model output
@@ -149,7 +170,7 @@ class SimpleGenerativeModel(
             resolved_optimizer_type = resolve_type(
                 self.optimizer_type, expected_super_type=Optimizer
             )
-            optimizer = resolved_optimizer_type(self.parameters(), lr=self.learning_rate)
+            optimizer = resolved_optimizer_type(self.model.parameters(), lr=self.learning_rate)
 
         if self.warmup_proportion > 0.0:
             stepping_batches = self.trainer.estimated_stepping_batches
