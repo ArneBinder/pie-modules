f8aa7ea3ec43a91255f04bf6853d68d584feb188
Arne Binder <arne.binder@dfki.de>
2024-02-27 20:20:19 +0100

    fix test names

--- tests/utils/test_span.py
+++ tests/utils/test_span.py
@@ -3,12 +3,12 @@ import pytest
 from pie_modules.utils.span import distance, distance_inner
 
 
-def test_inner_span_distance_overlap():
+def test_distance_inner_overlap():
     dist = distance_inner((0, 2), (1, 3))
     assert dist == -1
 
 
-def test_span_distance_unknown_type():
+def test_distance_unknown_type():
     with pytest.raises(ValueError) as excinfo:
         distance((0, 1), (2, 3), "unknown")
     assert str(excinfo.value) == "unknown distance_type=unknown. use one of: center, inner, outer"
