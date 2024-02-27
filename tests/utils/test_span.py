be5f6c9e7cbca873f6035ea18ade33d4e97d954b
Arne Binder <arne.binder@dfki.de>
2024-02-27 20:56:03 +0100

    add identical test case

--- tests/utils/test_span.py
+++ tests/utils/test_span.py
@@ -25,6 +25,8 @@ def test_have_overlap():
     # overlap, same end
     assert have_overlap((0, 2), (1, 2))
     assert have_overlap((1, 2), (0, 2))
+    # overlap, identical
+    assert have_overlap((0, 1), (0, 1))
 
 
 def test_distance_center():
@@ -43,6 +45,8 @@ def test_distance_center():
     # overlap, same end
     assert distance_center((0, 2), (1, 2)) == 0.5
     assert distance_center((1, 2), (0, 2)) == 0.5
+    # overlap, identical
+    assert distance_center((0, 1), (0, 1)) == 0.0
 
 
 def test_distance_inner():
@@ -61,9 +65,13 @@ def test_distance_inner():
     # overlap, same end
     assert distance_inner((0, 2), (1, 2)) == -1.0
     assert distance_inner((1, 2), (0, 2)) == -1.0
+    # overlap, identical
+    assert distance_inner((0, 1), (0, 1)) == -1.0
 
 
 def test_distance_outer():
+    # identical
+    assert distance_outer((0, 1), (0, 1)) == 1.0
     # no overlap, not touching
     assert distance_outer((0, 1), (2, 3)) == 3.0
     assert distance_outer((2, 3), (0, 1)) == 3.0
