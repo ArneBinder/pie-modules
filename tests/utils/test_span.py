f5fa3cdc6c76f767703ff1ff314c6dbcc03b9a65
Arne Binder <arne.binder@dfki.de>
2024-02-27 20:46:32 +0100

    add test_distance_center(), test_distance_inner(), test_distance_outer() and improve test_distance()

--- tests/utils/test_span.py
+++ tests/utils/test_span.py
@@ -1,14 +1,79 @@
 import pytest
 
-from pie_modules.utils.span import distance, distance_inner
+from pie_modules.utils.span import (
+    distance,
+    distance_center,
+    distance_inner,
+    distance_outer,
+)
 
 
-def test_distance_inner_overlap():
-    dist = distance_inner((0, 2), (1, 3))
-    assert dist == -1
+def test_distance_center():
+    # no overlap, not touching
+    assert distance_center((0, 1), (2, 3)) == 2.0
+    assert distance_center((2, 3), (0, 1)) == 2.0
+    # no overlap, touching
+    assert distance_center((0, 1), (1, 2)) == 1.0
+    assert distance_center((1, 2), (0, 1)) == 1.0
+    # overlap, not touching
+    assert distance_center((0, 2), (1, 3)) == 1.0
+    assert distance_center((1, 3), (0, 2)) == 1.0
+    # overlap, same start
+    assert distance_center((0, 2), (0, 3)) == 0.5
+    assert distance_center((0, 3), (0, 2)) == 0.5
+    # overlap, same end
+    assert distance_center((0, 2), (1, 2)) == 0.5
+    assert distance_center((1, 2), (0, 2)) == 0.5
 
 
-def test_distance_unknown_type():
-    with pytest.raises(ValueError) as excinfo:
-        distance((0, 1), (2, 3), "unknown")
-    assert str(excinfo.value) == "unknown distance_type=unknown. use one of: center, inner, outer"
+def test_distance_inner():
+    # no overlap, not touching
+    assert distance_inner((0, 1), (2, 3)) == 1.0
+    assert distance_inner((2, 3), (0, 1)) == 1.0
+    # no overlap, touching
+    assert distance_inner((0, 1), (1, 2)) == 0.0
+    assert distance_inner((1, 2), (0, 1)) == 0.0
+    # overlap, not touching
+    assert distance_inner((0, 2), (1, 3)) == -1.0
+    assert distance_inner((1, 3), (0, 2)) == -1.0
+    # overlap, same start
+    assert distance_inner((0, 2), (0, 3)) == -2.0
+    assert distance_inner((0, 3), (0, 2)) == -2.0
+    # overlap, same end
+    assert distance_inner((0, 2), (1, 2)) == -1.0
+    assert distance_inner((1, 2), (0, 2)) == -1.0
+
+
+def test_distance_outer():
+    # no overlap, not touching
+    assert distance_outer((0, 1), (2, 3)) == 3.0
+    assert distance_outer((2, 3), (0, 1)) == 3.0
+    # no overlap, touching
+    assert distance_outer((0, 1), (1, 2)) == 2.0
+    assert distance_outer((1, 2), (0, 1)) == 2.0
+    # overlap, not touching
+    assert distance_outer((0, 2), (1, 3)) == 3.0
+    assert distance_outer((1, 3), (0, 2)) == 3.0
+    # overlap, same start
+    assert distance_outer((0, 2), (0, 3)) == 3.0
+    assert distance_outer((0, 3), (0, 2)) == 3.0
+    # overlap, same end
+    assert distance_outer((0, 2), (1, 2)) == 2.0
+    assert distance_outer((1, 2), (0, 2)) == 2.0
+
+
+@pytest.mark.parametrize(
+    "distance_type",
+    ["outer", "center", "inner", "unknown"],
+)
+def test_distance(distance_type):
+    start_end = (0, 1)
+    other_start_end = (2, 3)
+    if distance_type != "unknown":
+        distance(start_end, other_start_end, distance_type)
+    else:
+        with pytest.raises(ValueError) as excinfo:
+            distance(start_end, other_start_end, distance_type)
+        assert (
+            str(excinfo.value) == "unknown distance_type=unknown. use one of: center, inner, outer"
+        )
