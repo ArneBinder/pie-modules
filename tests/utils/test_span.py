0a0ea102ebcf92ca61047b04e6cf8bfb8a424e36
Arne Binder <arne.binder@dfki.de>
2024-02-27 20:11:14 +0100

    add span utils

---
+++ tests/utils/test_span.py
@@ -0,0 +1,14 @@
+import pytest
+
+from pie_modules.utils.span import distance, distance_inner
+
+
+def test_inner_span_distance_overlap():
+    dist = distance_inner((0, 2), (1, 3))
+    assert dist == -1
+
+
+def test_span_distance_unknown_type():
+    with pytest.raises(ValueError) as excinfo:
+        distance((0, 1), (2, 3), "unknown")
+    assert str(excinfo.value) == "unknown distance_type=unknown. use one of: inner"
