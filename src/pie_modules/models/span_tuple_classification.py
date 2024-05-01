add tuple_indices_mask to model input and remove its calculation from the model; several fixes

src/pie_modules/models/span_tuple_classification.py
@@ -216,12 +216,18 @@ class SpanTupleClassificationModel(
 
         return span_embeddings
 
-    def tuple_pooler(self, span_embeddings: FloatTensor, tuple_indices: LongTensor) -> FloatTensor:
+    def tuple_pooler(
+        self,
+        span_embeddings: FloatTensor,
+        tuple_indices: LongTensor,
+        tuple_indices_mask: BoolTensor,
+    ) -> FloatTensor:
         """Pool the span embeddings for the tuples.
 
         Args:
             span_embeddings: The span embeddings. shape: (batch_size, num_spans, span_embedding_size)
             tuple_indices: The indices of the spans in the tuples. shape: (batch_size, num_tuples, num_tuple_entries)
+            tuple_indices_mask: A mask indicating which tuples are valid. shape: (batch_size, num_tuples)
 
         Returns:
             The pooled tuple embeddings. shape: (num_tuples_in_batch, num_tuple_entries * span_embedding_size)
@@ -231,18 +237,26 @@ class SpanTupleClassificationModel(
             raise ValueError(
                 f"Number of entries in tuple_indices should be equal to num_tuple_entries={self.num_tuple_entries}"
             )
-        # shape: (batch_size, num_tuples)
-        mask_valid_indices = self.get_valid_indices_mask(tuple_indices)
-        # number of entries per tuple
-        num_entries = tuple_indices.shape[-1]
-        # mask out invalid tuple indices
+        batch_size = tuple_indices.shape[0]
+        # we need to add the batch offsets to the tuple indices to get the correct indices in the
+        # flattened span_embeddings
+        batch_offsets = (
+            torch.arange(batch_size, device=tuple_indices.device).unsqueeze(-1) * batch_size
+        )
+        tuple_indices_with_offsets = tuple_indices + batch_offsets
         # shape: (num_tuples_in_batch, num_entries)
-        valid_tuple_indices = tuple_indices.view(-1, num_entries)[mask_valid_indices.view(-1)]
+        valid_tuple_indices_flat = tuple_indices_with_offsets[tuple_indices_mask]
+
+        # we need to flatten the span_embeddings to get the embeddings at the tuple indices
+        # shape: (batch_size * num_spans, span_embedding_size)
+        span_embeddings_flat = span_embeddings.view(-1, span_embeddings.size(-1))
 
         tuple_embeddings_list: List[FloatTensor] = []
-        for i in range(valid_tuple_indices.shape[-1]):
-            current_tuple_indices = valid_tuple_indices[:, :, i]
-            current_embeddings = get_embeddings_at_indices(span_embeddings, current_tuple_indices)
+        for i in range(valid_tuple_indices_flat.shape[-1]):
+            # shape: (num_tuples_in_batch)
+            current_tuple_indices = valid_tuple_indices_flat[:, i]
+            # shape: (num_tuples_in_batch, span_embedding_size)
+            current_embeddings = span_embeddings_flat[current_tuple_indices]
             tuple_embeddings_list.append(current_embeddings)
         if self.tuple_pooler_mode == "concat":
             tuple_embeddings = torch.cat(tuple_embeddings_list, dim=-1).to(span_embeddings.dtype)
@@ -250,28 +264,30 @@ class SpanTupleClassificationModel(
             raise ValueError(f"Invalid value for tuple_pooler_mode: {self.tuple_pooler_mode}")
         return tuple_embeddings
 
-    def get_valid_indices_mask(self, indices: LongTensor) -> BoolTensor:
-        # Mask out invalid tuple indices, i.e. tuples where all indices are below 0. These result from
-        # the padding. Note that either all indices are below 0 or all are above 0, so we can just check
-        # if the sum is greater than 0.
-        # shape: (batch_size, num_tuples)
-        return indices.sum(dim=-1) > 0
-
     def pooler(
-        self, hidden_states: FloatTensor, tuple_indices: LongTensor, **span_pooler_inputs
+        self,
+        hidden_states: FloatTensor,
+        tuple_indices: LongTensor,
+        tuple_indices_mask: BoolTensor,
+        **span_pooler_inputs,
     ) -> FloatTensor:
         # get the span embeddings from the hidden states and the start and end marker positions
         span_embeddings = self.span_pooler(hidden_states, **span_pooler_inputs)
         # get the tuple embeddings from the span embeddings and the tuple indices
         # this flattens the batch dimension!
-        tuple_embeddings = self.tuple_pooler(span_embeddings, tuple_indices)
+        tuple_embeddings = self.tuple_pooler(span_embeddings, tuple_indices, tuple_indices_mask)
         return tuple_embeddings
 
     def forward(self, inputs: InputType, targets: Optional[TargetType] = None) -> OutputType:
         pooler_inputs = {}
         model_inputs = {}
         for k, v in inputs.items():
-            if k in ["span_start_indices", "span_end_indices", "tuple_indices"]:
+            if k in [
+                "span_start_indices",
+                "span_end_indices",
+                "tuple_indices",
+                "tuple_indices_mask",
+            ]:
                 pooler_inputs[k] = v
             else:
                 model_inputs[k] = v
@@ -301,7 +317,7 @@ class SpanTupleClassificationModel(
             labels_flat = (probabilities_flat > self.multi_label_threshold).to(torch.long)
 
         # re-construct the original shape
-        mask_valid = self.get_valid_indices_mask(inputs["tuple_indices"])
+        mask_valid = inputs["tuple_indices_mask"]
         # create "empty" labels and probabilities tensors
         labels = torch.ones(mask_valid.shape, dtype=torch.long, device=labels_flat.device) * -100
         probabilities = (

src/pie_modules/taskmodules/re_span_pair_classification.py
@@ -48,7 +48,7 @@ from pytorch_ie.documents import (
 )
 from pytorch_ie.taskmodules.interface import ChangesTokenizerVocabSize
 from tokenizers import AddedToken
-from torch import LongTensor, Tensor
+from torch import BoolTensor, LongTensor, Tensor
 from torch.nn.utils.rnn import pad_sequence
 from torchmetrics import ClasswiseWrapper, F1Score, Metric, MetricCollection
 from transformers import AutoTokenizer
@@ -72,6 +72,7 @@ PAD_VALUES = {
     "span_end_indices": 0,
     "tuple_indices": -1,
     "labels": -100,
+    "tuple_indices_mask": False,
 }
 DTYPES = {
     "input_ids": torch.long,
@@ -80,6 +81,7 @@ DTYPES = {
     "span_end_indices": torch.long,
     "tuple_indices": torch.long,
     "labels": torch.long,
+    "tuple_indices_mask": torch.bool,
 }
 
 
@@ -95,6 +97,7 @@ class InputEncodingType(TypedDict, total=False):
     # list of lists of argument indices: [[head_idx, tail_idx], ...]
     # NOTE: these indices point into span_start_indices and span_end_indices!
     tuple_indices: LongTensor
+    tuple_indices_mask: BoolTensor
 
 
 class TargetEncodingType(TypedDict, total=False):
@@ -121,6 +124,7 @@ class ModelInputType(TypedDict, total=False):
     span_start_indices: LongTensor
     span_end_indices: LongTensor
     tuple_indices: LongTensor
+    tuple_indices_mask: BoolTensor
 
 
 class ModelTargetType(TypedDict, total=False):
@@ -619,6 +623,7 @@ class RESpanPairClassificationTaskModule(TaskModuleType, ChangesTokenizerVocabSi
                 "span_start_indices": span_start_indices,
                 "span_end_indices": span_end_indices,
                 "tuple_indices": tuple_indices,
+                "tuple_indices_mask": [True] * len(tuple_indices),
             }
             inputs_tensors = {k: to_tensor(k, v) for k, v in inputs.items()}
             task_encodings.append(

tests/taskmodules/test_re_span_pair_classification.py
@@ -160,6 +160,14 @@ def test_encode_input(task_encodings, document, taskmodule, cfg):
     if cfg == {}:
         assert len(task_encodings) == 1
         inputs = task_encodings[0].inputs
+        assert set(inputs) == {
+            "input_ids",
+            "attention_mask",
+            "span_start_indices",
+            "span_end_indices",
+            "tuple_indices",
+            "tuple_indices_mask",
+        }
         tokens = taskmodule.tokenizer.convert_ids_to_tokens(inputs["input_ids"])
         assert tokens == [
             "[CLS]",
@@ -209,10 +217,19 @@ def test_encode_input(task_encodings, document, taskmodule, cfg):
             ],
             [["[SPAN:ORG]", "I", "[/SPAN:ORG]"], ["[SPAN:ORG]", "H", "[/SPAN:ORG]"]],
         ]
+        assert inputs["tuple_indices_mask"].tolist() == [True, True, True]
     elif cfg == {"partition_annotation": "sentences"}:
         assert len(task_encodings) == 1
         for idx, encoding in enumerate(task_encodings):
             inputs = encoding.inputs
+            assert set(inputs) == {
+                "input_ids",
+                "attention_mask",
+                "span_start_indices",
+                "span_end_indices",
+                "tuple_indices",
+                "tuple_indices_mask",
+            }
             tokens = taskmodule.tokenizer.convert_ids_to_tokens(inputs["input_ids"])
             span_tokens = [
                 tokens[start:end]
@@ -247,7 +264,7 @@ def test_encode_input(task_encodings, document, taskmodule, cfg):
                         ["[SPAN:ORG]", "H", "[/SPAN:ORG]"],
                     ]
                 ]
-
+                assert inputs["tuple_indices_mask"].tolist() == [True]
             else:
                 raise ValueError(f"unexpected idx: {idx}")
     else:
@@ -349,6 +366,7 @@ def test_collate(taskmodule, task_encodings, cfg):
         "span_start_indices",
         "span_end_indices",
         "tuple_indices",
+        "tuple_indices_mask",
     }
     if cfg == {}:
         torch.testing.assert_close(
@@ -387,6 +405,7 @@ def test_collate(taskmodule, task_encodings, cfg):
         torch.testing.assert_close(inputs["span_start_indices"], tensor([[4, 12, 18]]))
         torch.testing.assert_close(inputs["span_end_indices"], tensor([[10, 15, 21]]))
         torch.testing.assert_close(inputs["tuple_indices"], tensor([[[0, 1], [0, 2], [2, 1]]]))
+        torch.testing.assert_close(inputs["tuple_indices_mask"], tensor([[True, True, True]]))
         assert set(targets) == {"labels"}
         torch.testing.assert_close(targets["labels"], tensor([[2, 3, 1]]))
     elif cfg == {"partition_annotation": "sentences"}:
@@ -397,21 +416,12 @@ def test_collate(taskmodule, task_encodings, cfg):
             ),
         )
         torch.testing.assert_close(
-            inputs["attention_mask"],
-            tensor([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]),
+            inputs["attention_mask"], tensor([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
         )
-        torch.testing.assert_close(
-            inputs["span_start_indices"],
-            tensor([[0, 8]]),
-        )
-        torch.testing.assert_close(
-            inputs["span_end_indices"],
-            tensor([[6, 11]]),
-        )
-        torch.testing.assert_close(
-            inputs["tuple_indices"],
-            tensor([[[0, 1]]]),
-        )
+        torch.testing.assert_close(inputs["span_start_indices"], tensor([[0, 8]]))
+        torch.testing.assert_close(inputs["span_end_indices"], tensor([[6, 11]]))
+        torch.testing.assert_close(inputs["tuple_indices"], tensor([[[0, 1]]]))
+        torch.testing.assert_close(inputs["tuple_indices_mask"], tensor([[True]]))
         assert set(targets) == {"labels"}
         torch.testing.assert_close(targets["labels"], tensor([[2]]))
     else: