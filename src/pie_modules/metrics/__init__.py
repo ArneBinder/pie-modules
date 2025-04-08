864bd0370ee842f59ed29435e4c4395f6fa2e5f5
Arne Binder <arne.binder@dfki.de>
2025-04-08 03:59:29 +0200

    add F1Metric

--- src/pie_modules/metrics/__init__.py
+++ src/pie_modules/metrics/__init__.py
@@ -1,5 +1,6 @@
 from pytorch_ie.metrics import F1Metric
 
+from .f1 import F1Metric
 from .relation_argument_distance_collector import RelationArgumentDistanceCollector
 from .span_coverage_collector import SpanCoverageCollector
 from .span_length_collector import SpanLengthCollector
