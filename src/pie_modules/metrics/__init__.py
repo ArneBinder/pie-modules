18673824ddf4a8d3109dd00dc72608cf92e5549b
Arne Binder <arne.binder@dfki.de>
2025-04-08 03:54:25 +0200

    adjust exports

--- src/pie_modules/metrics/__init__.py
+++ src/pie_modules/metrics/__init__.py
@@ -1,12 +1,12 @@
 from pytorch_ie.metrics import F1Metric
-from pytorch_ie.metrics.statistics import (
-    FieldLengthCollector,
-    LabelCountCollector,
-    SubFieldLengthCollector,
-    TokenCountCollector,
-)
 
 from .relation_argument_distance_collector import RelationArgumentDistanceCollector
 from .span_coverage_collector import SpanCoverageCollector
 from .span_length_collector import SpanLengthCollector
 from .squad_f1 import SQuADF1
+from .statistics import (
+    FieldLengthCollector,
+    LabelCountCollector,
+    SubFieldLengthCollector,
+    TokenCountCollector,
+)
