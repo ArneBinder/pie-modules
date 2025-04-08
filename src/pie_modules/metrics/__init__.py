3d248efc14dea008dd1f357bf8c30f507e1b31df
Arne Binder <arne.binder@dfki.de>
2025-04-08 04:01:48 +0200

    make pre-commit happy

--- src/pie_modules/metrics/__init__.py
+++ src/pie_modules/metrics/__init__.py
@@ -1,5 +1,3 @@
-from pytorch_ie.metrics import F1Metric
-
 from .f1 import F1Metric
 from .relation_argument_distance_collector import RelationArgumentDistanceCollector
 from .span_coverage_collector import SpanCoverageCollector
