b311f071a772af4f8669cce6f5dcbea8e3868cf7
rewo02 <redspottedbittern@gmail.com>
2025-04-11 12:25:40 +0200

    Add more tests for confusion matrix

--- tests/metrics/test_confusion_matrix.py
+++ tests/metrics/test_confusion_matrix.py
@@ -6,6 +6,7 @@ from pytorch_ie.core import AnnotationLayer, annotation_field
 from pytorch_ie.documents import TextBasedDocument
 
 from pie_modules.metrics import ConfusionMatrix
+import logging
 
 
 @pytest.fixture
@@ -58,3 +59,73 @@ def test_confusion_matrix(documents):
         "UNDETECTED": {"company": 1},
         "company": {"company": 1},
     }
+
+
+def test_undetected_is_gold_label(documents):
+    metric = ConfusionMatrix(layer="entities", undetected_label="animal")
+    with pytest.raises(ValueError) as exception:
+        metric(documents)
+
+    assert str(exception.value).startswith("The gold annotation has the label")
+
+
+def test_unassignable_is_pred_label(documents):
+    metric = ConfusionMatrix(layer="entities", unassignable_label="cat")
+    with pytest.raises(ValueError) as exception:
+        metric(documents)
+
+    assert str(exception.value).startswith("The predicted annotation has the label")
+
+
+@pytest.fixture
+def documents_with_several_gold_labels(documents):
+    doc1 = documents[0].copy()
+    doc2 = documents[1].copy()
+    doc1.entities.append(LabeledSpan(start=4, end=19, label="cat"))
+
+    return [doc1, doc2]
+
+
+def test_documents_with_several_gold_labels(documents_with_several_gold_labels, caplog):
+    metric = ConfusionMatrix(layer="entities")
+    with pytest.raises(ValueError):
+        metric(documents_with_several_gold_labels)
+
+    metric = ConfusionMatrix(layer="entities", strict=False)
+    metric(documents_with_several_gold_labels)
+    assert caplog.messages[0].startswith("The base annotation LabeledSpan(start=4, end=19, label='DUMMY_LABEL', score=1.0) has multiple gold labels")
+    # I only assert the start of the message here. The full warning includes the list of labels
+    # (e.g. ['cat', 'animal']), but the order of the list seems to me to be random.
+
+
+@pytest.fixture
+def documents_without_predictions(documents):
+    doc1 = documents[0].copy()
+    doc2 = documents[1].copy()
+
+    doc1.entities.predictions.clear()
+    doc2.entities.predictions.clear()
+
+    return [doc1, doc2]
+
+
+def test_documents_without_predictions(documents_without_predictions):
+    metric = ConfusionMatrix(layer="entities")
+    metric(documents_without_predictions)
+    assert dict(metric.counts) == {('animal', 'UNASSIGNABLE'): 2, ('company', 'UNASSIGNABLE'): 1}
+
+
+def test_show_as_markdown(documents, caplog):
+    caplog.set_level(logging.INFO)
+    metric = ConfusionMatrix(layer="entities", show_as_markdown=True)
+    metric(documents)
+
+    markdown = ['\nentities:\n|            |   animal |   cat |   company |\n|:-----------|---------:|------:|----------:|\n| animal     |        1 |     1 |         0 |\n| company    |        0 |     0 |         1 |\n| UNDETECTED |        0 |     0 |         1 |']
+
+    assert caplog.messages == markdown
+
+
+def test_annotation_processor_str(documents):
+    # annotation_processor = ""
+    # metric = ConfusionMatrix(layer="entities", annotation_processor=annotation_processor)
+    # Do you have any ideas for a good processor to use here?
