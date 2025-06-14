2a3f76b32b27d313c1262770483ee9903d39a5b2
Arne Binder <arne.binder@dfki.de>
2025-06-14 17:43:23 +0200

    add test_relation_statistics_mixin_show_statistics (taken from #191)

--- tests/taskmodules/common/test_mixins.py
+++ tests/taskmodules/common/test_mixins.py
@@ -1,9 +1,12 @@
 import dataclasses
+import logging
 from typing import List
 
 import torch
+from pytorch_ie import Annotation
 
 from pie_modules.taskmodules.common import BatchableMixin
+from pie_modules.taskmodules.common.mixins import RelationStatisticsMixin
 
 
 def test_batchable_mixin():
@@ -29,3 +32,45 @@ def test_batchable_mixin():
     )
     torch.testing.assert_close(batch["a"], torch.tensor([[1, 2, 3], [4, 5, 0]]))
     torch.testing.assert_close(batch["len_a"], torch.tensor([3, 2]))
+
+
+def test_relation_statistics_mixin_show_statistics(caplog):
+    """Test the RelationStatisticsMixin class."""
+
+    @dataclasses.dataclass
+    class Foo(RelationStatisticsMixin):
+        """A class that uses the RelationStatisticsMixin class."""
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+    @dataclasses.dataclass(eq=True, frozen=True)
+    class TestAnnotation(Annotation):
+        label: str
+        score: float = dataclasses.field(default=1.0, compare=False)
+
+    x = Foo(collect_statistics=True)
+
+    relations = [
+        TestAnnotation(label="A", score=1),
+        TestAnnotation(label="B", score=0.5),
+        TestAnnotation(label="C", score=0.3),
+    ]
+    x.collect_all_relations(kind="available", relations=relations)
+    x.collect_relation(kind="skipped_test", relation=relations[1])
+    x.collect_all_relations(
+        kind="used",
+        relations=set(x._collected_relations["available"])
+        - set(x._collected_relations["skipped_test"]),
+    )
+    with caplog.at_level(logging.INFO):
+        x.show_statistics()
+    assert caplog.messages[0] == (
+        "statistics:\n"
+        "|              |   A |   B |   C |   all_relations |\n"
+        "|:-------------|----:|----:|----:|----------------:|\n"
+        "| available    |   1 |   1 |   1 |               3 |\n"
+        "| skipped_test |   0 |   1 |   0 |               1 |\n"
+        "| used         |   1 |   0 |   1 |               2 |\n"
+        "| used %       | 100 |   0 | 100 |              67 |"
+    )
